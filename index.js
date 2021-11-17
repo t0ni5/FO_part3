3eb609d3e3aaf16c6dd934b947d327d2b8874c0e
Toni <aiming1high@gmail.com>
2021-11-17 16:52:45 +0300

    added production frontend build

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 app.use(express.json())
